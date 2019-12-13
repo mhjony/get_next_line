c14bb3055797b4fdfe2a1edd779889968abee9f7
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2019-12-13 22:07:33 +0200

    header file included

--- get_next_line.h
+++ get_next_line.h
@@ -3,21 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhasan <mhasan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jony <jony@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 14:54:17 by mhasan            #+#    #+#             */
-/*   Updated: 2019/11/08 19:54:42 by mhasan           ###   ########.fr       */
+/*   Updated: 2019/12/13 22:07:00 by jony             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# define BUFF_SIZE 10
+#define GET_NEXT_LINE_H
+#define BUFF_SIZE 10
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 
-int		get_next_line(const int fd, char **line);
-char	*ft_strnew(size_t size);
+int get_next_line(const int fd, char **line);
 
 #endif
