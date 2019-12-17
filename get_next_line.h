c7afba19f0d9eeb7ed99a892b09ccb1309b0caae
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2019-12-17 22:37:45 +0200

    fixed up the header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jony <jony@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 14:54:17 by mhasan            #+#    #+#             */
-/*   Updated: 2019/12/13 22:09:03 by jony             ###   ########.fr       */
+/*   Updated: 2019/12/17 22:35:40 by jony             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include "libft/libft.h"
 
 int get_next_line(const int fd, char **line);
 char *ft_strnew(size_t size);
